3503ff0b22e2ecfea8739ec020ede70d97bcafc4
Thien Trinh <thien.trinh@hcl.com>
2022-06-28 23:28:21 +0700

    Update unit test for Toast

--- src/__tests__/Toast.test.js
+++ src/__tests__/Toast.test.js
@@ -1,52 +1,58 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render, screen, within } from '@testing-library/react';
 import Toast from '../components/Toast/Toast';
 
-import * as Redux from 'react-redux';
+import { useSelector, useDispatch, Provider } from 'react-redux';
+import { createPortal } from 'react-dom';
 import store from '../store/index';
 
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
-  useSelector: jest.fn()
+  useSelector: jest.fn(),
+  useDispatch: jest.fn()
 }));
 
 jest.mock('react-dom', () => ({
   ...jest.requireActual('react-dom'),
-  createPortal: jest.fn((component, container) => component)
+  createPortal: jest.fn()
 }));
 
 const renderWithProvider = () => {
   return render(
-    <Redux.Provider store={store}>
+    <Provider store={store}>
       <Toast />
-    </Redux.Provider>
+    </Provider>
   );
 };
 
 describe('Navigation', () => {
+  beforeEach(() => {
+    useDispatch.mockReturnValue(() => {});
+    createPortal.mockImplementation((component) => component);
+  });
   afterAll(cleanup);
 
   it('should match snapshot for success', () => {
-    Redux.useSelector.mockReturnValueOnce({
+    useSelector.mockReturnValueOnce({
       status: 'success',
       title: 'Success',
       message: 'Success Message'
     });
-    const { asFragment } = renderWithProvider();
-    expect(asFragment(<Toast />)).toMatchSnapshot();
+    const { container } = renderWithProvider();
+    expect(within(container).getByRole('alert')).toMatchSnapshot();
   });
 
   it('should match snapshot for error', () => {
-    Redux.useSelector.mockReturnValueOnce({
+    useSelector.mockReturnValueOnce({
       status: 'error',
       title: 'error',
       message: 'Error Message'
     });
-    const { asFragment } = renderWithProvider();
-    expect(asFragment(<Toast />)).toMatchSnapshot();
+    const { container } = renderWithProvider();
+    expect(within(container).getByRole('alert')).toMatchSnapshot();
   });
 
   it('should not render toast when there is no notification', () => {
-    Redux.useSelector.mockReturnValueOnce(null);
+    useSelector.mockReturnValueOnce(null);
     expect(screen.queryByRole('alert')).not.toBeInTheDocument();
   });
 });
